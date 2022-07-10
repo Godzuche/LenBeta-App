2248601c845791670efee1d363d60a752c657592
Administrator <godxuche@gmail.com>
2022-07-10 02:26:13 +0100

    refactor and worked on the profile screen...

--- app/src/main/java/com/lenbeta/lenbetaapp/feature/user/student/home/profile/StudentProfileScreen.kt
+++ app/src/main/java/com/lenbeta/lenbetaapp/feature/user/student/home/profile/StudentProfileScreen.kt
@@ -1,24 +1,37 @@
 package com.lenbeta.lenbetaapp.feature.user.student.home.profile
 
-import androidx.compose.foundation.Image
+import android.net.Uri
+import androidx.activity.compose.ManagedActivityResultLauncher
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ChevronRight
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.FilterQuality
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -27,9 +40,14 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import coil.size.Size
 import com.lenbeta.lenbetaapp.R
 import com.lenbeta.lenbetaapp.core.ui.theme.LenBetaAppTheme
 import com.lenbeta.lenbetaapp.feature.util.CenteredSmallTopBar
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -94,40 +112,165 @@ fun ProfileSectionCard(title: Int, value: String, modifier: Modifier = Modifier)
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProfileSectionExpandableCard(
+    title: Int,
+    items: List<String>,
+    modifier: Modifier = Modifier,
+    listState: LazyListState
+) {
+    val scope = rememberCoroutineScope()
+    var isExpanded by remember { mutableStateOf(false) }
+    Card(
+        onClick = {
+            isExpanded = !isExpanded
+            if (isExpanded) {
+                scope.launch {
+                    delay(100)
+                    listState.animateScrollToItem(7)
+                }
+            }
+        },
+        modifier = Modifier
+            .then(modifier)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            contentAlignment = Alignment.CenterStart
+        ) {
+            Column(
+                modifier = Modifier
+                    .wrapContentHeight()
+                    .padding(horizontal = 16.dp)
+                    .animateContentSize(),
+                verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+                CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                    Text(
+                        text = stringResource(title),
+                        style = MaterialTheme.typography.labelLarge
+                    )
+                }
+                if (items.isNotEmpty()) {
+                    CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.high) {
+                        Text(
+                            text = items.first(),
+                            style = MaterialTheme.typography.bodyLarge,
+                            fontWeight = FontWeight.SemiBold,
+                            fontSize = 16.sp
+                        )
+                    }
+                    AnimatedVisibility(
+                        visible = isExpanded,
+                        enter = expandVertically(
+                            animationSpec = spring(
+                                dampingRatio = Spring.DampingRatioMediumBouncy,
+                                stiffness = Spring.StiffnessMedium
+                            )
+                        ),
+                        exit = shrinkVertically(
+                            animationSpec = spring(
+                                dampingRatio = Spring.DampingRatioMediumBouncy,
+                                stiffness = Spring.StiffnessMedium
+                            )
+                        )
+                    ) {
+                        Column(
+                            verticalArrangement = Arrangement.spacedBy(4.dp),
+                            modifier = Modifier
+                                .wrapContentHeight()
+//                                .animateContentSize()
+                        ) {
+                            for (index in 1 until items.size) {
+                                CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.high) {
+                                    Text(
+                                        text = items[index],
+                                        style = MaterialTheme.typography.bodyLarge,
+                                        fontWeight = FontWeight.SemiBold,
+                                        fontSize = 16.sp
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            OutlinedIconButton(
+                onClick = { isExpanded = !isExpanded },
+                modifier = Modifier.align(Alignment.TopEnd),
+                border = null
+            ) {
+                Icon(
+                    imageVector = if (isExpanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                    contentDescription = null
+                )
+            }
+        }
+    }
+}
+
 @Composable
 fun StudentProfileScreen(modifier: Modifier = Modifier) {
-    Column(
+    val listState = rememberLazyListState()
+    LazyColumn(
         horizontalAlignment = Alignment.CenterHorizontally,
+        state = listState,
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(top = 16.dp, bottom = 8.dp, start = 8.dp, end = 8.dp),
         modifier = Modifier
             .fillMaxSize()
             .then(modifier)
-            .verticalScroll(state = rememberScrollState())
-
     ) {
-        Spacer(modifier = Modifier.height(16.dp))
-        ProfileHeader()
-        Spacer(modifier = Modifier.height(32.dp))
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(8.dp),
-            modifier = Modifier.padding(horizontal = 8.dp)
-        ) {
+        item(0) { ProfileHeader() }
+        item(1) { Spacer(modifier = Modifier.height(24.dp)) }
+        item(2) {
             ProfileSectionCard(
                 title = R.string.name,
                 value = "God'swill Jonathan"
             )
+        }
+        item(3) {
             ProfileSectionCard(
                 title = R.string.student_email,
                 value = "Godswill.jonathan@ust.edu.ng"
             )
+        }
+        item(4) {
             ProfileSectionCard(
                 title = R.string.engineering,
                 value = "Chemical/Petrochemical Engineering"
             )
+        }
+        item(5) {
             ProfileSectionCard(
                 title = R.string.level,
                 value = "400L"
             )
+        }
+        /*    item(6) {
+                ProfileSectionCard(
+                    title = R.string.skills,
+                    value = "Android Development"
+                )
+            }*/
+        item(6) {
+            ProfileSectionExpandableCard(
+                title = R.string.skills,
+                items = listOf(
+                    "Android Development",
+                    "Technical Writing",
+                    "Public Speaking",
+                    "Git/GitHub",
+                    "Machine Learning"
+                ),
+                listState = listState
+            )
+        }
+        item(7) {
+            Spacer(modifier = Modifier.height(16.dp))
         }
     }
 }
@@ -139,41 +282,7 @@ fun ProfileHeader(modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
     ) {
-        Box(
-            modifier = modifier,
-            contentAlignment = Alignment.Center
-        ) {
-            Image(
-                painter = painterResource(R.drawable.avatar),
-                contentDescription = "Profile picture",
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .size(156.dp)
-                    .clip(CircleShape)
-            )
-            Box(
-                modifier = Modifier
-                    .clip(RoundedCornerShape(100))
-                    .align(Alignment.BottomEnd)
-                    .background(MaterialTheme.colorScheme.background)
-                    .padding(1.dp)
-
-            ) {
-                FilledIconButton(
-                    onClick = {},
-                    modifier = Modifier
-                        .align(Alignment.BottomEnd),
-                    colors = IconButtonDefaults.iconButtonColors(
-                        containerColor = MaterialTheme.colorScheme.tertiaryContainer
-                    )
-                ) {
-                    Icon(
-                        imageVector = Icons.Filled.Edit,
-                        contentDescription = "Edit button"
-                    )
-                }
-            }
-        }
+        ProfileImage(modifier)
         Spacer(modifier = Modifier.height(16.dp))
         CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.high) {
             Text(
@@ -186,6 +295,92 @@ fun ProfileHeader(modifier: Modifier = Modifier) {
             Text(
                 text = "godswill.jonathan@ust.edu.ng",
                 style = MaterialTheme.typography.titleSmall
+            )
+        }
+    }
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+private fun ProfileImage(modifier: Modifier) {
+    var imageUri by remember {
+        mutableStateOf<Uri?>(null)
+    }
+    val context = LocalContext.current
+/*    val bitmap = remember {
+        mutableStateOf<Bitmap?>(null)
+    }*/
+    val galleryLauncher =
+        rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) { uri: Uri? ->
+            imageUri = uri
+        }
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center
+    ) {
+/*        *//*     imageUri?.let {
+                 if (Build.VERSION.SDK_INT < 28) {
+                     bitmap.value = MediaStore.Images.Media.getBitmap(context.contentResolver, it)
+                 } else {
+                     val source = ImageDecoder.createSource(context.contentResolver, it)
+                     bitmap.value = ImageDecoder.decodeBitmap(source)
+                 }
+                 *//**//*bitmap.value?.let { btm ->
+                Image(
+                    bitmap = btm.asImageBitmap(),
+                    contentDescription = "Profile picture",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(156.dp)
+                        .clip(CircleShape)
+                )
+            }*//**//*
+        }*/
+        val imageRequest = ImageRequest.Builder(context)
+            .data(imageUri ?: R.drawable.avatar)
+            .size(Size.ORIGINAL)
+            .crossfade(true)
+            .build()
+        /*val painter = rememberAsyncImagePainter(
+            model = model,
+            contentScale = ContentScale.Crop
+        )*/
+        AsyncImage(
+            model = imageRequest,
+            placeholder = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = "Profile picture",
+            contentScale = ContentScale.Crop,
+            filterQuality = FilterQuality.High,
+            modifier = Modifier
+                .size(156.dp)
+                .clip(CircleShape)
+        )
+        ProfileImageEditButton(galleryLauncher)
+    }
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+private fun BoxScope.ProfileImageEditButton(galleryLauncher: ManagedActivityResultLauncher<String, Uri?>) {
+    Box(
+        modifier = Modifier
+            .clip(RoundedCornerShape(100))
+            .align(Alignment.BottomEnd)
+            .background(MaterialTheme.colorScheme.background)
+            .padding(1.dp)
+
+    ) {
+        FilledIconButton(
+            onClick = { galleryLauncher.launch("image/*") },
+            modifier = Modifier
+                .align(Alignment.BottomEnd),
+            colors = IconButtonDefaults.iconButtonColors(
+                containerColor = MaterialTheme.colorScheme.tertiaryContainer
+            )
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Edit,
+                contentDescription = "Edit button"
             )
         }
     }
