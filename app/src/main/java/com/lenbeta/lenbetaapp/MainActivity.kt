3f556f522c15c3072bfec782416563d96dd088f8
Godzuche <godxuche@gmail.com>
2023-01-29 05:06:36 +0100

    modified to use uiState stateflow from MainActivityViewModel

--- app/src/main/java/com/lenbeta/lenbetaapp/MainActivity.kt
+++ app/src/main/java/com/lenbeta/lenbetaapp/MainActivity.kt
@@ -6,33 +6,56 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.lenbeta.lenbetaapp.MainActivityUiState.Loading
+import com.lenbeta.lenbetaapp.MainActivityUiState.Success
 import com.lenbeta.lenbetaapp.core.ui.theme.LenBetaAppTheme
 import com.lenbeta.lenbetaapp.feature.LenBetaApp
-import com.lenbeta.lenbetaapp.feature.SplashViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 
 @ExperimentalMaterial3Api
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    //    @Inject
-//    lateinit var splashViewModel: SplashViewModel
-    private val splashViewModel: SplashViewModel by viewModels()
-
+    private val viewModel: MainActivityViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen().setKeepOnScreenCondition {
-            val isLoading by splashViewModel.isLoading
-            isLoading
-        }
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
+
+        var uiState: MainActivityUiState by mutableStateOf(Loading)
+
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState
+                    .onEach {
+                        uiState = it
+                    }
+                    .collect()
+            }
+        }
+
+        splashScreen.setKeepOnScreenCondition {
+            when (uiState) {
+                Loading -> true
+                is Success -> false
+
+            }
+        }
+
         setContent {
             LenBetaAppTheme {
-                val startScreen by splashViewModel.startDestination
-                startScreen?.let {
+                if (uiState is Success) {
                     LenBetaApp(
-                        startDestination = it
+                        startDestination = (uiState as Success).startDestination
                     )
                 }
             }
